99c877b93cbba8ace690550c0e18bd471d2e530e
Ahmad-Arslan-10 <arslanimtiaz849@gmail.com>
2025-05-16 00:37:02 +0500

    Initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Collection from './pages/Collection'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Product from './pages/Product'
+import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
 
 const App = () => {
   return (
-    <div>Hello!</div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/product/:productid' element={<Product/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/placeOrders' element={<PlaceOrder/>} />
+        <Route path='/login' element={<Login/>} />
+    </Routes>
+    </div>
   )
 }
 
